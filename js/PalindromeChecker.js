d06cef4256b8cfa32fbb2902c913fd58f1fcbe93
Amando Filipe <eu@amando-filipe.com>
2015-02-06 17:00:25 +0000

    Forgot to remove PalindromeChecker prototype from within the IIFE

--- js/PalindromeChecker.js
+++ js/PalindromeChecker.js
@@ -1,16 +1,14 @@
-;(function(){
-    var PalindromeChecker = function(){
-    };
-    
-    PalindromeChecker.prototype = {
-        check: function(str){
-            str = str.replace(/\s/g, '').replace(/\W+/g, '').toLowerCase();
-            reversed = str.split("").reverse().join("");
-            if(str === reversed){                
-                return true;
-            } else {
-                 return false;   
-            }
+var PalindromeChecker = function(){
+};
+
+PalindromeChecker.prototype = {
+    check: function(str){
+        str = str.replace(/\s/g, '').replace(/\W+/g, '').toLowerCase();
+        reversed = str.split("").reverse().join("");
+        if(str === reversed){                
+            return true;
+        } else {
+             return false;   
         }
-    };  
-})();
+    }
+}; 
